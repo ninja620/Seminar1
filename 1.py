9939b4ca370ce6731b80df1a84669bf55d676951
ninja620 <73035619+ninja620@users.noreply.github.com>
2025-09-27 09:20:36 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,7 @@
+a = int(input())
+c=0
+while c != a:
+    c+=1
+    print(c)
+    
+    
---
+++ 2.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = int(input())
+"""c=[]
+c.append(a)
+c.append(b)
+print(max(c))"""
+if a>b:
+    print(a)
+else:
+    print(b)
+    
+    
